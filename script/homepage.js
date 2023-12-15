49ff607edb8c72eaa2b5e7c4bc28d364130f06b1
Vaibhav Upadhyay <rkovaibhav007@gmail.com>
2023-12-15 19:54:35 +0530

    added photos and review section with JS

--- script/homepage.js
+++ script/homepage.js
@@ -1,17 +1,31 @@
 menuItemsHTML();
-let menuItemsInnerText
+reviewsHTML();
 
 function menuItemsHTML() {
   let bodyelement = document.querySelector('.menu-items-list');
   pizzaItems.forEach(pizzaitem => {
     bodyelement.innerHTML += `<div class="menu-item-container">
-    <div class="item-image-div"><img class="item-image" src="images/main-image.jpg" alt="imagenotfound"></div>
+    <div class="item-image-div"><img class="item-image" src=${pizzaitem.image} alt="imagenotfound"></div>
     <div class="item-name">
       ${pizzaitem.productName}
     </div>
     <div class="item-description">${pizzaitem.productDescription}</div>
     <div class="item-rating">⭐${pizzaitem.productRatings}</div>
     <button class="item-order-button">Order Now</button>
+  </div>`
+  });
+ 
+}
+
+function reviewsHTML() {
+  let bodyelement = document.querySelector('.reviews');
+  pizzaReviews.forEach(pizzareview => {
+    bodyelement.innerHTML += `<div class="review-container">
+    <div class="review-text">${pizzareview.reviewText}</div>
+    <div>
+    <div class="review-stars">⭐${pizzareview.rating}</div>
+    <div class="review-customer">${pizzareview.name}</div>
+    </div>
   </div>`
   });
  
--- script/pizzaitems.js
+++ script/pizzaitems.js
@@ -4,69 +4,79 @@ const pizzaItems = [
       productName: "Margherita",
       productPrice: 799.99,
       productRatings: 4.5,
-      productDescription: "Classic pizza with tomato, mozzarella, and basil toppings."
+      productDescription: "Classic pizza with tomato, mozzarella, and basil toppings.",
+      image:"images/margherita.jpg"
   },
   {
       productId: 2,
       productName: "Pepperoni",
       productPrice: 899.99,
       productRatings: 4.8,
-      productDescription: "Pizza topped with pepperoni slices and melted cheese."
+      productDescription: "Pizza topped with pepperoni slices and melted cheese.",
+      image:"images/pepperonipizza.jpg"
   },
   {
       productId: 3,
       productName: "Vegetarian",
       productPrice: 849.99,
       productRatings: 4.3,
-      productDescription: "Loaded with a variety of fresh vegetables and cheese."
+      productDescription: "Loaded with a variety of fresh vegetables and cheese.",
+      image:"images/vegeterianpizza.jpg"
   },
   {
       productId: 4,
       productName: "BBQ Chicken",
       productPrice: 929.99,
       productRatings: 4.6,
-      productDescription: "Delicious barbecue chicken pizza with red onions and cilantro."
+      productDescription: "Delicious barbecue chicken pizza with red onions and cilantro.",
+      image:"images/bbqchicken.jpg"
   },
   {
       productId: 5,
       productName: "Supreme",
       productPrice: 999.99,
       productRatings: 4.7,
-      productDescription: "The ultimate pizza with a mix of meats, veggies, and extra cheese."
+      productDescription: "The ultimate pizza with a mix of meats, veggies, and extra cheese.",
+      image:"images/supremepizza.jpg"
   },
   {
       productId: 6,
       productName: "Mushroom Lovers",
       productPrice: 849.99,
       productRatings: 4.2,
-      productDescription: "For those who can't get enough of savory mushrooms on their pizza."
+      productDescription: "For those who can't get enough of savory mushrooms on their pizza.",
+      image:"images/mushroompizza.jpg"
   },
   {
       productId: 7,
       productName: "Buffalo Chicken",
       productPrice: 899.99,
       productRatings: 4.4,
-      productDescription: "Spicy buffalo chicken pizza with blue cheese crumbles."
+      productDescription: "Spicy buffalo chicken pizza with blue cheese crumbles.",
+      image:"images/buffalochicken.jpg"
   },
   {
       productId: 8,
       productName: "Pesto Delight",
       productPrice: 849.99,
       productRatings: 4.1,
-      productDescription: "Pesto sauce, cherry tomatoes, and fresh basil make this pizza a delight."
+      productDescription: "Pesto sauce, cherry tomatoes, and fresh basil make this pizza a delight.",
+      image:"images/pestodelight.jpg"
   },
   {
       productId: 9,
       productName: "Four Cheese",
       productPrice: 949.99,
       productRatings: 4.5,
-      productDescription: "A cheesy masterpiece with a blend of four delicious cheeses."
+      productDescription: "A cheesy masterpiece with a blend of four delicious cheeses.",
+      image:"images/fourcheese.jpg"
   },
   {
       productId: 10,
       productName: "Meat Lovers",
       productPrice: 999.99,
       productRatings: 4.8,
-      productDescription: "Packed with a variety of savory meats for the ultimate carnivore's delight."
+      productDescription: "Packed with a variety of savory meats for the ultimate carnivore's delight.",
+      image:"images/meatlovers.jpg"
   }
 ]
